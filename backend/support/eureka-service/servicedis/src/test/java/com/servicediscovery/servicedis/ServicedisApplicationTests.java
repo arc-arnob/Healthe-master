1debf9177a55f78980c12f74cfa327a0719aabb6
Arnob Chowdhury <as9025@srmist.edu.in>
2021-01-18 11:16:11 +0530

    configured zuul service

---
+++ backend/support/eureka-service/servicedis/src/test/java/com/servicediscovery/servicedis/ServicedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicediscovery.servicedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- backend/support/reddit/src/main/java/com/redditb/reddit/RedditApplication.java
+++ backend/support/reddit/src/main/java/com/redditb/reddit/RedditApplication.java
@@ -2,11 +2,13 @@ package com.redditb.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @SpringBootApplication
 @EnableAsync
+@EnableEurekaClient
 public class RedditApplication {
 
 	public static void main(String[] args) {
